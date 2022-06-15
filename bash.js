d8c43a3e9c51ddeaf21a6aab1ce80a251cb1a627
Donna-Sayos <Donnasayos@gmail.com>
2022-06-15 18:54:07 -0400

    finished

--- bash.js
+++ bash.js
@@ -1,12 +1,33 @@
-process.stdout.write('prompt > ');
+const pwd = require('./pwd')
+const ls = require('./ls');
+
+// const cmd = data.toString().trim();
+
+// pwd()
+// ls()
+
+const writePrompt = function(){
+    process.stdout.write('\nprompt > ');
+}
+
+writePrompt()
+
 process.stdin.on('data', (data) => {
+    
     const cmd = data.toString().trim();
 
-    if(cmd === 'pwd') {
-        process.stdout.write(process.cwd());
+    if (cmd === 'pwd') {
+        pwd()
+        writePrompt();
+    } else if (cmd === "ls") {
+        ls(writePrompt)
+       
     }
-
-    // process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
+    else {
+        process.stdout.write('You typed: ' + cmd);
+        writePrompt()
+    }
+    
+    
 });
 
